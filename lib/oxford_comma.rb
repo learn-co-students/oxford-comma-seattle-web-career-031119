f68cd4c3833a13cebb77ce9d7f5c3c1b69d66df6
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 20:15:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    result = array[0]
+  elsif array.size == 2
+    result = array.join(" and ")
+  else 
+    result = array.join(", ")
+    result.insert(result.rindex(",")+1, " and")
+  end
+  result
 end
